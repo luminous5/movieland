acbe47bce45927ce8105670e51c529008d206de0
Alexandra Mirtcheva <alexandramirtcheva@Alexandras-MacBook-Pro.local>
2024-03-17 12:09:11 +0100

    placeholder for images

--- src/App.js
+++ src/App.js
@@ -27,14 +27,37 @@ const App = () => {
     return (
        <div className="app">
         <h1>Movie Search</h1>
-        {loading ? 
+        <div className='search'>
+            <input placeholder='Search for movies'
+                value="Superman"
+                onChange={() => {}}
+            />
+            <img src={SearchIcon}
+                alt="Search"
+                onClick={() => {}} 
+            />
+                
+        </div>
+        <div className="container">
+            <div className="movie">
+                <div>
+                    <p>
+                        {movie.Year}
+                    </p>
+                </div>
+                <div>
+                    <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder/400'} alt={movie.Title}/>
+                </div>
+            </div>
+        </div>
+        {/* {loading ? 
             (
                 <p>Loading data ...</p>
             ) : 
             (
                 <p>Data : {JSON.stringify(data)}</p>
             )
-        }
+        } */}
        </div>
     );
 }
